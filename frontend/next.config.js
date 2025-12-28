6bae97fb90689e8f4579ff0ed9fd588be3f1be21
Pronostici Master <pronostici@master.com>
2025-12-28 18:19:35 +0100

    Add webpack alias configuration to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['media.api-sports.io'], // For team logos
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    }
+    return config
   },
   async rewrites() {
     return [
